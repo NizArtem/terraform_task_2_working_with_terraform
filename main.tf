d1f2395da3ca0d53a5acce8d2712966a6fe4b15e
Artem Nizamiiev <upuyayua@gmail.com>
2025-12-18 20:47:09 +0200

    solved2

--- main.tf
+++ main.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.105.0"
-    }
-
-    archive = {
-      source  = "hashicorp/archive"
-      version = "~> 2.5"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "example" {
   name     = var.storage_account_name
   location = var.location
